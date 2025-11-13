deddce9b537d386fdbb44740d050adbb8c3d79c6
anshmunjapara <anshmunjapara@gmail.com>
2025-11-12 20:37:41 -0600

    add bootstrap to ejs files

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from "express";
+import path from "path";
 import pg from "pg";
+import { fileURLToPath } from "url"; // Import fileURLToPath
+
+// Define __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
@@ -16,8 +22,21 @@ db.connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve Bootstrap CSS
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+
+// Serve Bootstrap JS
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+
+// Serve Popper.js
+app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')));
+
+
 app.get("/", (req, res) => {
-  res.render("index.ejs", {title: "Home", message: "Working"});
+  res.render("book.ejs", {title: "Home", message: "Working"});
 });
 
 app.listen(port, () => {
